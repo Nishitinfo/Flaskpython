69802c22514a6f0e4f900d45f30756b49aa4e803
infobizzsVishnu <jahnvi@infobizzs.com>
2020-02-19 14:51:48 +0530

    changes

--- View.py
+++ View.py
@@ -9,17 +9,34 @@ app = Flask(__name__)
 def home():
     if not session.get('logged_in'):
         return render_template('Registration.html')
+
     else:
-        return "Hello Boss! Logout"
+       # Check if user is loggedin
+      if 'loggedin' in session :
+                # User is loggedin show them the home page
+                return render_template('home.html', username=session['username'])
+            # User is not loggedin redirect to login page
+    return login()
 
 @app.route('/Registration', methods=['POST'])
 def Registration():
     if request.form['password'] == 'admin' and request.form['username'] == 'admin':
         session['logged_in'] = True
-        return render_template('Login.html')
+        return login()
+        # return render_template('Login.html')
     else:
         flash('wrong password!')
         return home()
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.form['password'] == 'admin' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+        return render_template('')
+    else:
+        flash('wrong password!')
+        return home()
+
 
 @app.route("/logout")
 def logout():
--- login.py
+++ login.py
@@ -1,38 +1,48 @@
 
 from flask import Flask, render_template, redirect, url_for, request
-import pymysql.cursors
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 import re
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     msg = ''
     error=''
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'root'
+    app.config['MYSQL_DB'] = 'Test'
+    mysql = MySQL(app)
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form :
-
-        username = request.form['username']
-        password = request.form['password']
-        # Check if account exists using MyS
-        cursor = pymysql.connection.cursor(pymysql.cursors.DictCursor)
-        cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
-        users = cursor.fetchone()
-        if users :
-            session['loggedin'] = True
-            session['id'] = users['id']
-            session['username'] = users['username']
-            # Redirect to home page
-            return 'Logged in successfully!'
-        else :
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+    # Create variables for easy access
+         username = request.form['username']
+         password = request.form['password']
+    # Check if account exists using MySQL
+         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+         cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+    # Fetch one record and return result
+         users = cursor.fetchone()
+    # If account exists in accounts table in out database
+         if account :
+        # Create session data, we can access this data in other routes
+          session['loggedin'] = True
+          session['id'] = users['id']
+          session['username'] = users['username']
+        # Redirect to home page
+          return 'Logged in successfully!'
+    else :
+        # Account doesnt exist or username/password incorrect
+        msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
 
     return render_template('Login.html', error=error)
 
 
-
-if __name__ == "__main__" :
-    app.run(host='127.0.0.2', port=8000, debug=True)
+#
+# if __name__ == "__main__" :
+#     app.run(host='127.0.0.2', port=8000, debug=True)
 
 
 
